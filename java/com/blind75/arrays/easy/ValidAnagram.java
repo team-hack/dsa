c9eb7fe8621a7bfd4e6c6aa5ff1c3517136bd170
karpra <karpra@karpra.me>
2022-05-20 11:12:22 -0700

    added optmial solution

--- java/com/blind75/arrays/easy/ValidAnagram.java
+++ java/com/blind75/arrays/easy/ValidAnagram.java
@@ -3,15 +3,24 @@ package com.blind75.arrays.easy;
 import com.blind75.helper.Task;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram extends Task {
     @Override
     protected void run() {
         String s = "anagram";
         String t = "nagaram";
-        System.out.println("bruteForce ==>" + bruteForce(s, t));
+        //System.out.println("bruteForce ==>" + bruteForce(s, t));
+        System.out.println("optimal ==>" + optimal(s, t));
     }
 
+    /**
+     * if string is ascii based O(N) for time and space complexity
+     * @param s
+     * @param t
+     * @return
+     */
     private boolean bruteForce(String s, String t) {
         if (s.length() != t.length())
             return false;
@@ -21,7 +30,31 @@ public class ValidAnagram extends Task {
             si[s.charAt(i) - 'a']++;
             ti[t.charAt(i) - 'a']++;
         }
-        return Arrays.equals(si,ti);
+        return Arrays.equals(si, ti);
+    }
+
+    /**
+     * if string is unicode based
+     * O(N) for time and space complexity
+     * @param s
+     * @param t
+     * @return
+     */
+    private boolean optimal(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        if (s.length() != t.length())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.toCharArray()[i];
+            char d = t.toCharArray()[i];
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            map.put(d, map.getOrDefault(d, 0) - 1);
+        }
+        for (Map.Entry<Character, Integer> set : map.entrySet()
+        ) {
+            if (set.getValue() != 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
