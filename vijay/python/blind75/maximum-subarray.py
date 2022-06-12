55849032a4cec1397583a3116067f3d9914f51f5
Vijay Xtreme <vijay@Vijays-MacBook-Pro.local>
2022-06-11 17:45:32 -0700

    Max subarray - check right side and left side

--- vijay/python/blind75/maximum-subarray.py
+++ vijay/python/blind75/maximum-subarray.py
@@ -5,7 +5,7 @@ A subarray is a contiguous part of an array.
 """
 from typing import List
 # Examples
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+nums = [-2, 1, -3, 8, -1, 2, 1, -5, 4]
 nums1 = [5, 4, -1, 7, 8]
 nums2 = [1]
 
@@ -37,16 +37,36 @@ def max_sub_array(nums: List[int]) -> List:
     start = max(nums)
     max_num = start[0]
     start_index = start[1]
-    end_index = -1
+    left_end_index = -1
+    right_end_index = -1
     sum = 0
 
+    # go left from the max number
     for i in range(start_index, 0, -1):
         sum += nums[i]
         if sum >= max_num:
-            end_index = i
+            left_end_index = i
+            max_num = sum
+
+    #print("left_index", left_end_index)
+
+    # go right from the max number -1
+    for i in range(start_index+1, len(nums)):
+        sum += nums[i]
+        if sum >= max_num:
+            right_end_index = i
+            max_num = sum
+
+    #print("right_index", right_end_index)
 
     new_sum = 0
-    for i in range(start_index, end_index-1, -1):
+    if left_end_index <= 0:
+        left_end_index = start_index
+
+    if right_end_index <= 0:
+        right_end_index = start_index
+
+    for i in range(left_end_index, right_end_index+1):
         new_sum += nums[i]
 
     return new_sum
